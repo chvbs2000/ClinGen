e6073df85865963a567fc2420394ce1a18c6e4cc
Kai-yu Chen <chvbs2000@gatech.edu>
2019-10-08 11:43:12 -0700

    add version.py

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 from datetime import date
 
 
-def get_release():
+def get_release(self):
 	"""
 	return the most updated version
 	"""
-	return date.today().strftime("-%Y-%m-%d")
+	return "str(date.today().strftime("-%Y-%m-%d"))"
