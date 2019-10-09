7c5023e0d2bc3489c860e6ac014a9a976c4bd8f8
Kai-yu Chen <chvbs2000@gatech.edu>
2019-10-09 11:40:05 -0700

    debug version.py

--- version.py
+++ version.py
@@ -10,7 +10,7 @@ def get_release(self):
     
     #client = requests.Session()
     #self.client
-    responese = self.client.head(self.SRC_URLS[0])
+    response = self.client.head(self.SRC_URLS[0])
     text = response.headers["Content-Disposition"]
     date = re.findall(r'\d{4}-\d\d-\d\d',text)
 
