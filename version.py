5efe0c60c5384fed0b87cf0a7f23d89802f6891b
Kai-yu Chen <chvbs2000@gatech.edu>
2019-10-09 12:16:32 -0700

    debug version.py

--- version.py
+++ version.py
@@ -1,13 +1,12 @@
-import requests
-import re
-
 def get_release(self):
-
     """
     return the most updated version
     """
-    client = self.client
-    response = client.head(self.SRC_URLS[0])
+    
+    import requests
+    import re
+    
+    response = self.client.head(self.SRC_URLS[0])
     text = response.headers["Content-Disposition"]
     date = re.findall(r'\d{4}-\d\d-\d\d',text)
 
